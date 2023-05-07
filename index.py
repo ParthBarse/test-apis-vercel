1234d101790d89dae254ffd9280cdba07739dece
Parth Barse <91686761+ParthBarse@users.noreply.github.com>
2023-05-08 02:34:46 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -100,12 +100,15 @@ def addbalance():
         print(logged_user)
         if logged_user:
             prev_bal = logged_user["balance"]
-            new_bal = int(prev_bal)+int(addMoney)
-            new_data ={"$set": {
-                "balance":new_bal
-            }}
-            users.update_one({"uid":uid}, new_data)
-            return {"isSuccess":"True", "details":{"balance":new_bal,"uid":logged_user["uid"]}}
+            if int(prev_bal) < 10000:
+                new_bal = int(prev_bal)+int(addMoney)
+                new_data ={"$set": {
+                    "balance":new_bal
+                }}
+                users.update_one({"uid":uid}, new_data)
+                return {"isSuccess":"True", "details":{"balance":new_bal,"uid":logged_user["uid"]}}
+            else:
+                return {"isSuccess":"False", "msg":"Cannot add more than Rs 10,000"}
         else:
             return {"isSuccess":"False", "msg":"Invalid Data"}
     
